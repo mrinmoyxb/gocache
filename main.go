d51f85fe0dde2551f3ecb955f24a471e13f83a20
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-08-23 19:58:53 +0530

    added functions with Locks

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Cache struct {
+	mu sync.RWMutex
 	store map[string]string
 }
 
@@ -13,15 +17,24 @@ func NewCache() *Cache {
 }
 
 func (c *Cache) Set(key string, value string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	
 	c.store[key] = value
 }
 
 func (c *Cache) Get(key string) (string, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	
 	val, exists := c.store[key]
 	return val, exists
 }
 
 func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	
 	delete(c.store, key)
 }
 
@@ -29,6 +42,38 @@ func (c *Cache) FetchAll() map[string]string {
 	return c.store
 }
 
-func main() {
-	
+func main(){
+	cache := NewCache()
+	var wg sync.WaitGroup
+
+	for i:=0; i<10; i++ {
+		wg.Add(1)
+		go func(id int){
+			defer fmt.Println("Done writing")
+			defer wg.Done()
+			key := fmt.Sprintf("user_%d", id)
+			val := fmt.Sprintf("value_%d", id)
+			fmt.Println("SET: ",key, val)
+			cache.Set(key, val)
+		}(i)
+	}
+
+	wg.Wait()
+
+	for i:=0; i<10; i++ {
+		wg.Add(1)
+		go func(id int){
+			defer fmt.Println("Done reading")
+			defer wg.Done()
+			key := fmt.Sprintf("user_%d", id)
+			value, ok := cache.Get(key)
+			if !ok{
+				fmt.Println("error")
+			}
+			fmt.Println("GET: ",key, value)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Successfully Processed 200 concurrent operations")
 }
